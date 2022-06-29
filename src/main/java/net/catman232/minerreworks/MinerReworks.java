df96b58d56811502ade7fd5da3196cd002c9fa8f
Catman-232 <jameswelch232@gmail.com>
2022-06-29 13:44:44 +0100

    the comment test

--- src/main/java/net/catman232/minerreworks/MinerReworks.java
+++ src/main/java/net/catman232/minerreworks/MinerReworks.java
@@ -3,7 +3,7 @@ package net.catman232.minerreworks;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//comment test
 public class MinerReworks implements ModInitializer {
 	public static final String MOD_ID = "minerreworks";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
